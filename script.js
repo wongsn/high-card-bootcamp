9ce74c4eee95c5d82b27c6049ac7d3dff23d2a2b
michelle-mok <michellemokhuihui@gmail.com>
2021-06-29 19:19:46 +0800

    first commit base solution

--- script.js
+++ script.js
@@ -1,1 +1,213 @@
-// Please implement exercise logic here
+/*####################
+## HELPER FUNCTIONS ##
+####################*/
+
+// Get a random index ranging from 0 (inclusive) to max (exclusive).
+const getRandomIndex = (max) => Math.floor(Math.random() * max);
+
+// Shuffle an array of cards
+const shuffleCards = (cards) => {
+  // Loop over the card deck array once
+  for (let currentIndex = 0; currentIndex < cards.length; currentIndex += 1) {
+    // Select a random index in the deck
+    const randomIndex = getRandomIndex(cards.length);
+    // Select the card that corresponds to randomIndex
+    const randomCard = cards[randomIndex];
+    // Select the card that corresponds to currentIndex
+    const currentCard = cards[currentIndex];
+    // Swap positions of randomCard and currentCard in the deck
+    cards[currentIndex] = randomCard;
+    cards[randomIndex] = currentCard;
+  }
+  // Return the shuffled deck
+  return cards;
+};
+
+const makeDeck = () => {
+  // Initialise an empty deck array
+  const newDeck = [];
+  // Initialise an array of the 4 suits in our deck. We will loop over this array.
+  const suits = ['hearts', 'diamonds', 'clubs', 'spades'];
+
+  // Loop over the suits array
+  for (let suitIndex = 0; suitIndex < suits.length; suitIndex += 1) {
+    // Store the current suit in a variable
+    const currentSuit = suits[suitIndex];
+
+    // initialise variable suitSymbol
+    let currentSymbol;
+
+    // set suit symbol to match current suit
+    if (currentSuit === 'hearts') {
+        currentSymbol = '♥️';
+    } else if (currentSuit === 'spades') {
+        currentSymbol = '♠️';
+    } else if (currentSuit === 'clubs') {
+        currentSymbol = '♣️';
+    } else {
+        currentSymbol = '♦️';
+    }
+
+    // set the color of the card (used later to determine the css class which in turn determines the color)
+    // does not directly set the color of the card
+    let cardColor;
+    if (currentSymbol === '♥️' || currentSymbol === '♦️' ) {
+        cardColor = 'red';
+    } else {
+        cardColor = 'black';
+    }
+
+    // Loop from 1 to 13 to create all cards for a given suit
+    // Notice rankCounter starts at 1 and not 0, and ends at 13 and not 12.
+    // This is an example of a loop without an array.
+    for (let rankCounter = 1; rankCounter <= 13; rankCounter += 1) {
+      // By default, the card name is the same as rankCounter
+      let cardName = `${rankCounter}`;
+
+      // If rank is 1, 11, 12, or 13, set cardName to the ace or face card's name
+      if (cardName === '1') {
+        cardName = 'A';
+      } else if (cardName === '11') {
+        cardName = 'J';
+      } else if (cardName === '12') {
+        cardName = 'Q';
+      } else if (cardName === '13') {
+        cardName = 'K';
+      }
+
+      // Create a new card with the current name, suit, suit symbol, display name colour and rank
+      const cardInfo = {
+        suitSymbol: currentSymbol,
+        suit: currentSuit,
+        name: cardName,
+        color: cardColor,
+        rank: rankCounter,
+        };
+      // Add the new card to the deck
+      newDeck.push(cardInfo);
+    }
+  }
+
+  // Return the completed card deck
+  return newDeck;
+};
+
+// build the card display 
+const createCard = (cardInfo) => {
+  const suit = document.createElement('div');
+  suit.classList.add('suit', cardInfo.color);
+  suit.innerText = cardInfo.suitSymbol;
+
+  const name = document.createElement('div');
+  name.classList.add(cardInfo.name, cardInfo.color);
+  name.innerText = cardInfo.name;
+
+  const card = document.createElement('div');
+  card.classList.add('card');
+
+  card.appendChild(name);
+  card.appendChild(suit);
+
+  return card;
+};
+
+// Create a helper function for output to abstract complexity
+// of DOM manipulation away from game logic
+const output = (message) => {
+  gameInfo.innerText = message;
+};
+
+// helper function for getting 3 cards from the deck 
+const getPlayerHand = (playerHand) => {
+    for (let i = 0; i < 3; i += 1) {
+    playerCard = deck.pop();
+    playerHand.push(playerCard);
+    }
+}
+
+/*####################
+## GLOBAL VARIABLES ##
+####################*/
+const deck = shuffleCards(makeDeck());
+
+let playersTurn = 1; // matches with starting instructions
+// let player1Card;
+
+let cardContainer;
+
+const player1Button = document.createElement('button');
+
+const player2Button = document.createElement('button');
+
+const gameInfo = document.createElement('div');
+
+
+/* ##########################
+## PLAYER ACTION CALLBACKS ##
+###########################*/
+const player1Click = () => {
+  if (playersTurn === 1) {
+    // getting player 1's hand
+    let player1Hand = [];
+    getPlayerHand(player1Hand);
+    console.log('player 1 hand', player1Hand);
+    // Create card element from card metadata
+    const cardElement = createCard(player1Card);
+    // Empty cardContainer in case this is not the 1st round of gameplay
+    cardContainer.innerHTML = '';
+    // Append the card element to the card container
+    cardContainer.appendChild(cardElement);
+    playersTurn = 2;
+  }
+};
+
+const player2Click = () => {
+  if (playersTurn === 2) {
+     // getting player 2's hand
+    let player2Hand = [];
+    getPlayerHand(player2Hand);
+    console.log('player 2 hand', player2Hand);
+    // Create card element from card metadata
+    const cardElement = createCard(player2Card);    
+    // Append card element to card container
+    cardContainer.appendChild(cardElement);
+    
+    // Switch to player 1's turn
+    playersTurn = 1;
+
+    if (player1Card.rank > player2Card.rank) {
+      output('player 1 wins');
+    } else if (player1Card.rank < player2Card.rank) {
+      output('player 2 wins');
+    } else {
+      output('tie');
+    }
+  }
+};
+
+/*#######################
+## GAME INITIALISATION ##
+#######################*/
+const initGame = () => {
+    cardContainer = document.createElement('div');
+    cardContainer.classList.add('card-container');
+    document.body.appendChild(cardContainer);
+    // initialize button functionality
+    player1Button.innerText = 'Player 1 Draw';
+    document.body.appendChild(player1Button);
+
+    player2Button.innerText = 'Player 2 Draw';
+    document.body.appendChild(player2Button);
+
+    player1Button.addEventListener('click', player1Click);
+    player2Button.addEventListener('click', player2Click);
+
+    // fill game info div with starting instructions
+    gameInfo.innerText = 'Its player 1 turn. Click to draw a card!';
+    document.body.appendChild(gameInfo);
+};
+
+//############################################################
+
+// call the function that will initialise gameplay
+initGame();
